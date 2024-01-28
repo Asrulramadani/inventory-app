b9d6d102cdd706a16df0af977009229bd311f183
Asrulramadani <asrulramadani0711@gmail.com>
2024-01-28 23:17:29 +0800

    add logo

--- resources/views/template/partials/aside.blade.php
+++ resources/views/template/partials/aside.blade.php
@@ -1,7 +1,10 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
       <a href="{{route('dashboard')}}" class="app-brand-link">
-        <span class="app-brand-text demo menu-text fw-bolder ms-2">Inventory App</span>
+        <span class="app-brand-logo demo">
+            <img src="{{url('/assets/img/icons/logo.png')}}" width="40px" alt="">
+        </span>
+        <span class="app-brand-text demo menu-text fw-bolder ms-2">Inventory</span>
       </a>
 
       <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
